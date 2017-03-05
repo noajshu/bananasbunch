91e742b99aed6fd00ba942d46a29ce9619f5abc5
Noah Shutty <noahshutty@gmail.com>
2017-03-04 20:07:51 -0800

    added working widget

---
+++ widget/views/widget.php
@@ -0,0 +1,8 @@
+<!-- This file is used to markup the public-facing widget. -->
+this is a widget <h1>HI GUYS</h1>
+<?php
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+?>
--- wp-plugin/readme.txt
+++
@@ -1,7 +0,0 @@
-=== Weeb Hello World ===
-Contributors: Noah Shutty
-Website: http://bananasbunch.org
-Tags: hello world
-Requires at least: 2.7
-Stable tag: 1.0
-
